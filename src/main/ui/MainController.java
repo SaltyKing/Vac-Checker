772ea2245dbceffd5663de7bfb4a3b8a7a60293f
SaltyKing <ben.zett11@gmail.com>
2018-02-22 16:50:47 +0100

    View erstellt

--- src/main/ui/MainController.java
+++ src/main/ui/MainController.java
@@ -1,5 +1,26 @@
 package main.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class MainController {
+
+	@FXML
+	private TableView<?> tvListe;
+
+    @FXML
+    private TableColumn<?, ?> tcName;
+
+    @FXML
+    private TableColumn<?, ?> tcStatus;
+
+    @FXML
+    private Button btHinzufügen;
+
+    @FXML
+    private TextField tfHinzufügen;
 
 }
