71d9b14fcdfe0ef058ef7d907e3ce97d50d08d8a
SaltyKing <ben.zett11@gmail.com>
2018-02-22 16:39:43 +0100

    Erstes Fenster erstellt

--- src/main/Start.java
+++ src/main/Start.java
@@ -1,5 +1,29 @@
 package main;
 
-public class Start {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+
+public class Start extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("ui/MainView.fxml"));
+			primaryStage.setTitle("Vac Checker");
+			Scene scene = new Scene(root, 500, 500);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+			primaryStage.setResizable(false);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
