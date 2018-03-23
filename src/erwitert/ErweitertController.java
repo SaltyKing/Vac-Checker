f34ad6c9ca2017c0f9876e34b0d15a9c82663606
SaltyKing <ben.zett11@gmail.com>
2018-03-23 23:20:31 +0100

    Oberfläche erstellt

---
+++ src/erwitert/ErweitertController.java
@@ -0,0 +1,20 @@
+package erwitert;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ErweitertController {
+
+    @FXML
+    private Label lbName;
+
+    @FXML
+    private Label lbLink;
+
+    @FXML
+    private Label lbStatus;
+
+    @FXML
+    private Label lbHinzugefügt;
+
+}
