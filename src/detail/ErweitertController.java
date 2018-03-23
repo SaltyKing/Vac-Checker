e6c3570116da910a87302c6ab467050637d39a31
SaltyKing <ben.zett11@gmail.com>
2018-03-23 23:14:46 +0100

    Aufräumen

---
+++ src/detail/ErweitertController.java
@@ -0,0 +1,29 @@
+package detail;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+public class ErweitertController {
+
+    @FXML
+    private Label lbDatum;
+
+    @FXML
+    private Label lbName;
+
+    @FXML
+    private Button btHinzufügen;
+
+    @FXML
+    private TextArea taBeschreibung;
+
+    @FXML
+    private Hyperlink hpUrl;
+
+    @FXML
+    private Label lbStatus;
+
+}
